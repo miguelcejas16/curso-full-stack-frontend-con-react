0b8d7a440e6df8f69e43da01fcb5de2a8a758864
miguelcejas16 <161471830+miguelcejas16@users.noreply.github.com>
2025-02-28 14:16:45 -0300

    Boton añadido en la review
    Boton en el footer que cambia de color el mismo

--- sprint1-practico3/src/components/Footer.jsx
+++ sprint1-practico3/src/components/Footer.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 function Footer() {
+  const [color, setColor] = useState("bg-blue-600");
+
+  const cambiarColor = () => {
+    setColor(color === "bg-blue-600" ? "bg-red-600" : "bg-blue-600")
+  }
+
     return (
-      <footer className="bg-blue-600 text-white text-center p-4 overlock-regular">
+      <footer className={`${color} text-white text-center p-4 overlock-regular`}>
         <p>&copy; 2025 Mi Landing Page. Miguel Cejas Romero. Todos los derechos reservados.</p>
+        <button onClick={cambiarColor}>Click para cambiar de color</button>
       </footer>
     );
   }
